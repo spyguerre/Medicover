ee53d1e306ea4eb27c0a29ec4314321e9efe1a20
Alexandre <alexandre.padellini@telecomnancy.eu>
2025-11-17 15:55:44 +0100

    Init

---
+++ app/testpage/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      {/* Empty content area for now */}
+    </div>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+    return (
+      <nav className="sidebar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    );
+  }
